72f32785cb90c46f8bf09d14b26edbbd17cf9c45
hrdhruv <harshdhruv889@gmail.com>
2025-11-12 20:34:45 +0530

    test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Calculator {
-    // Square root testing purpose
+    // Square root 
     public double sqrt(double x) {
         if (x < 0) throw new IllegalArgumentException("sqrt: negative input");
         return Math.sqrt(x);
